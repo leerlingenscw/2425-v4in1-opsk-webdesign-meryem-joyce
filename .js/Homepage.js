0fdcd726586e3db3b2c45334f5d4422d2bdf68ba
Joyce <135309@stanislascollege.net>
2025-01-06 20:29:33 +0000

    Tiny adjustment

--- .js/Homepage.js
+++ .js/Homepage.js
@@ -1,7 +1,7 @@
-// Get the button:
+
+// Back to top button
 let mybutton = document.getElementById("topbutton");
                 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -12,7 +12,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
