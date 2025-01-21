96d2af60eb3dc62310f7b0f8ee672cff69e9b162
Joyce <135309@stanislascollege.net>
2025-01-21 07:57:01 +0000

    Added visitor counter

--- javascript/Homepage.js
+++ javascript/Homepage.js
@@ -15,3 +15,16 @@ function scrollFunction() {
 function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Website visit counter
+var counterContainer = document.querySelector(".counter");
+var visitCount = localStorage.getItem("page_view");
+
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
