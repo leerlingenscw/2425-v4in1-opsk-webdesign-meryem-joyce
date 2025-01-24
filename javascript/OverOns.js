1090b668f94a6fa5bb7f182f4f6014376d1f9a8e
Joyce <135309@stanislascollege.net>
2025-01-24 08:23:05 +0000

    Added js

--- javascript/OverOns.js
+++ javascript/OverOns.js
@@ -1,3 +1,4 @@
+// Animation //
 let words = document.querySelectorAll(".word")
 words.forEach(word => {
   let letters = word.textContent.split("")
@@ -38,3 +39,34 @@ let rotateText = () => {
 
 rotateText()
 setInterval(rotateText, 4000)
+
+
+// Back to top button
+let mybutton = document.getElementById("topbutton");
+                
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 180) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// Website visit counter
+var counterContainer = document.querySelector(".counter");
+var visitCount = localStorage.getItem("page_view");
+
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
